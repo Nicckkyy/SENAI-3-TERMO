3a4a44a0b9cf96454c903462bf65900c17dbec9f
Nicckkyy <nicolecamachorose873@gmail.com>
2025-08-20 15:51:40 -0300

    28.08.2025

---
+++ front/kanban/src/App.jsx
@@ -0,0 +1,13 @@
+import { BarraNavegacao } from "./components/BarraNavegacao"
+import './styles/main.scss';
+
+function App() {
+
+  return (
+    
+      <BarraNavegacao/>
+    
+  )
+}
+
+export default App
---
+++ front/kanban/src/components/BarraNavegacao.jsx
@@ -0,0 +1,11 @@
+export function BarraNavegacao(){
+    return(
+        <nav className="barra"> 
+            <ul>
+                <li>Cadatro de Usuarios</li>
+                <li>Cadatro de tarefas</li>
+                <li>Gerenciamento de tarefas</li>
+            </ul>
+        </nav>
+    )
+}
